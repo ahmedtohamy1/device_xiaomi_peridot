b7d50ccec82c87532a44d910ee2bc6a7255c6e7b
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    peridot: Enable freeform multi-windows
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FastCharge
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Fingerprint
 TARGET_HAS_UDFPS := true
 
