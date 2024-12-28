1b88868b81117da9d11a6982dcc559742364db06
raghavt20 <raghavt20@aospa.co>
2024-12-28 20:13:47 +0530

    peridot: Update from V816.0.10.0.UNPMIXM
    Change-Id: If356cc20fc4c9251535b953f2cfe0e0b656fb575

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -27,10 +27,12 @@ PRODUCT_SYSTEM_NAME := peridot_global
 PRODUCT_SYSTEM_DEVICE := peridot
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.9.0.UNPMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.10.0.UNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys \
+    DeviceName=peridot \
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
 
-BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.9.0.UNPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
