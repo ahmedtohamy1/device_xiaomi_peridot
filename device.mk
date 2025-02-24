715689d4ae867151c20199879a11fd4ee316af33
Guidix <guidixm@gmail.com>
2025-02-24 17:08:03 +0200

    peridot: Build Sensors sub hal from source
    this reverts commit: 634eb13c4eb50aba6db00f8aace216f8241fc8d3
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -469,7 +469,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
-    sensors.xiaomi.peridot
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
